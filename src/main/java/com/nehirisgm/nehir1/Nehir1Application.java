6bd0e6c353a3058c96ae75d3beada96ddf140125
yazilimsaka <yazilim@sakagrup.com>
2023-06-01 21:23:03 +0300

    Initial commit

---
+++ src/main/java/com/nehirisgm/nehir1/Nehir1Application.java
@@ -0,0 +1,13 @@
+package com.nehirisgm.nehir1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nehir1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nehir1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nehirisgm/nehir1/Nehir1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nehirisgm.nehir1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nehir1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
